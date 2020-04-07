641bc26334482f835ab745cb061969ee3cd1c619
liaoYQ1115 <liaoyq1115@163.com>
2020-04-07 17:12:18 +0800

    注册模块-图形码完成

--- src/view/login/register.vue
+++ src/view/login/register.vue
@@ -8,7 +8,7 @@
     <div slot="title" class="title">用户注册</div>
     <!-- 表单 -->
     <el-form :model="form" label-width="100px" ref="form" :rules="rules">
-        <!-- 将头像加入验证 -->
+        <!-- 将头像加入验证  name=请求参数-->
       <el-form-item label="头像" prop="avatar"> 
         <el-upload
           class="avatar-uploader"
@@ -22,6 +22,29 @@
           <img v-if="imageUrl" :src="imageUrl" class="avatar" />
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
+      </el-form-item>
+      
+      <el-form-item  label="昵称" prop="username">
+          <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item  label="邮箱" prop="email">
+          <el-input v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item  label="手机" prop="phone">
+          <el-input v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item  label="密码" prop="password">
+          <el-input v-model="form.password" :show-password="true"></el-input>
+      </el-form-item>
+      <el-form-item label="图形码" prop="code">
+          <el-row>
+              <el-col :span="16">
+                   <el-input v-model="form.code"></el-input>
+              </el-col>
+              <el-col :span="7" :offset="1">
+                  <img src="@/assets/img/key.jpg" alt class="code"/>
+              </el-col>
+          </el-row>
       </el-form-item>
     </el-form>
 
@@ -42,9 +65,49 @@ export default {
        //   表单数据
       form: {
         avatar:'', //头像地址
+        username:'', //用户名
+        email:'',//邮箱
+        phone:'', //手机
+        password:'',//密码
+        code:'',//图形码
       },
       rules:{ //表单验证规则绑定
-        avatar:[{required:true,message:'请上传头像',trigger:'change'}]
+        avatar:[{required:true,message:'请上传头像',trigger:'change'}],
+        username:[{required:true,message:'请输入昵称',trigger:'change'}],
+        email:[
+            {required:true,message:'请输入邮箱地址',trigger:'change'},
+            {   //validator 自定义校验
+                validator:(rule,value,callback)=>{
+                    let _reg = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+                    if(_reg.test(value)){
+                        callback();
+                    }else{
+                        callback('请正确输入邮箱地址');
+                    }
+                }
+            }
+        ],
+        phone:[
+            {required:true,message:'请输入手机号',trigger:'change'},
+            {   //validator 自定义校验
+                validator:(rule,value,callback)=>{
+                    let _reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+                    if(_reg.test(value)){
+                        callback();
+                    }else{
+                        callback('请正确输入手机号');
+                    }
+                }
+            }
+        ],
+        password:[
+            {required:true,message:'请输入密码',trigger:'change'},
+            {min:6,max:12,message:'请输入6-12位的密码',trigger:'change'}
+        ],
+        code:[
+            {required:true,message:'请输入图形码',trigger:'change'},
+            {min:4,max:4,message:'请输入4位数图形码',trigger:'change'}
+        ],
       }
     };
   },
@@ -64,7 +127,7 @@ export default {
     },
     //上传成功后
     handleAvatarSuccess(res) {
-        console.log(res); //接口返回值
+        // console.log(res); //接口返回值
     //   this.imageUrl = URL.createObjectURL(file.raw); //保存图片地址 方法一
     this.imageUrl=this.baseUrl+'/'+res.data.file_path; //保存图片地址 方法二/常用
     this.form.avatar=res.data.file_path; //上传成功后须在form表单数据里面保存一下该图像avatar值
@@ -128,6 +191,11 @@ export default {
   }
   .footer{
       text-align: center;
+  }
+  .code{
+      height: 40px;
+      width: 100%;
+      border:1px dashed #ccc;
   }
 }
 </style>
